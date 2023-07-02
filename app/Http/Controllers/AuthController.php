1456f3f75751eee30d56f0e91e18c20159a47dec
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-07-02 23:37:43 +0700

    penambahan controller login

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function showLoginForm()
+    {
+        return view('auth.login');
+    }
+    public function login(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            // Login berhasil
+            if (Auth::user()->isAdmin()) {
+                return redirect('/');
+            } else {
+                // return redirect('/');
+                echo "Gagal Login";
+            }
+        } else {
+            // Login gagal
+            return back()->withErrors([
+                'email' => 'Invalid credentials.',
+            ]);
+        }
+    }
+    public function logout(Request $request)
+    {
+        Auth::logout();
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+    }
+}
