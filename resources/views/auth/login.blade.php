3504a5ff47d28a878b9c8f84127b9caa5b346403
Thecaracter <92218072+Thecaracter@users.noreply.github.com>
2023-08-07 00:51:59 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,17 +48,17 @@
 
                     <div class="text-center p-t-12">
                         <span class="txt1">
-                            Forgot
+
                         </span>
                         <a class="txt2" href="/forgot-password">
-                            Username / Password?
+
                         </a>
                     </div>
 
                     <div class="text-center p-t-136">
-                        <a class="txt2" href="/register">
-                            Create your Account
-                            <i class="fa fa-long-arrow-right m-l-5" aria-hidden="true"></i>
+                        <a class="txt2" href="">
+
+
                         </a>
                     </div>
                 </form>
